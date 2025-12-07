b6acf942ee17167699f891cacc8929041ee69fae
Sidnei Barbosa <sidneibarbosa17@gmail.com>
2025-12-07 17:01:02 -0300

    fix: adding extension

--- frontend/src/pages/PlayoffSelector.jsx
+++ frontend/src/pages/PlayoffSelector.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import TeamFlag from '../components/TeamFlag';
+import TeamFlag from '../components/TeamFlag.jsx';
 
 const PlayoffSelector = () => {
   const navigate = useNavigate();
